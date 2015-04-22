ffb18784527fe38c071ed722bcf6c5eb9c1eb2a1
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2015-04-23 00:48:31 +0200

    Make tool thread-safe

--- pinpredict/pinpredict.cpp
+++ pinpredict/pinpredict.cpp
@@ -22,39 +22,38 @@ typedef UINT32 u32;
 typedef unsigned long long u64;
 typedef unsigned long Addr;
 
-#if DEBUG_STATS
-#define MAX_PREDCOUNTERS 50
-u64 predCounters[50];
-#endif
 
 struct AddrPredict {
   Addr currentAddr;
   Addr predictedAddr;
   INT32 stride;
 
-  Addr iaddr;
-  Addr lastAddr;
   INT32 lastStride;
+  Addr lastAddr;
   UINT32 missCount;
   UINT32 count;
 };
 
-#define MAXPREDS 1000000
-struct AddrPredict apreds[MAXPREDS];
+#define MAX_PREDS 100000
+Addr predIAddr[MAX_PREDS];
 int nextAPred = 0;
 
-#define MAX_EXITS 100000
+#define MAX_EXITS 50000
 int exit_from[MAX_EXITS];
 int exit_to[MAX_EXITS];
-u64 exit_counter[MAX_EXITS];
 int nextExit = 0;
 
-#define MAX_TRACEPREDS 50
-#define MAX_THREADS 20
+// for DEBUG_STATS
+#define MAX_PREDCOUNTERS 50
 
 struct ThreadData {
-    Addr addrBuf[MAX_TRACEPREDS];
+    struct AddrPredict apreds[MAX_PREDS];
+    u64 exit_counter[MAX_EXITS];
+    u64 predCounters[MAX_PREDCOUNTERS];
 };
+
+#define MAX_THREADS 50
+struct ThreadData* tdata[MAX_THREADS];
 
 REG predReg;
 
@@ -64,6 +63,11 @@ REG predReg;
 
 /* ===================================================================== */
 // Callbacks
+
+void* returnPredPtr(THREADID tid, UINT off)
+{
+    return &(tdata[tid]->apreds[off]);
+}
 
 template<int N>
 void* returnPredPtrN(struct AddrPredict* p)
@@ -110,13 +114,14 @@ void check(struct AddrPredict* p, Addr a)
     //		    p->addr, p->stride);
 }
 
-void afterExit(UINT32 off, UINT32 next)
+void afterExit(THREADID tid, UINT32 off, UINT32 next)
 {
 #if DEBUG_STATS
-  predCounters[next-off]++;
+  tdata[tid]->predCounters[next-off]++;
+  if (off == next) return;
 #endif
 
-  struct AddrPredict* p = &apreds[off];
+  struct AddrPredict* p = &(tdata[tid]->apreds[off]);
   while(1) {
     check(p, p->currentAddr);
     off++;
@@ -126,10 +131,10 @@ void afterExit(UINT32 off, UINT32 next)
 }
 
 template<int N>
-void afterExitN(UINT32 off)
+void afterExitN(THREADID tid, UINT32 off)
 {
   struct AddrPredict* p;
-  p = &apreds[off];
+  p = &(tdata[tid]->apreds[off]);
   check(p, p->currentAddr);
   for(int n=1; n<N; n++) {
     p++;
@@ -137,33 +142,62 @@ void afterExitN(UINT32 off)
   }
 }
 
-void incExitCounter(int off)
+void incExitCounter(THREADID tid, int off)
 {
-  exit_counter[off]++;
+  tdata[tid]->exit_counter[off]++;
 }
+
+
+/* ===================================================================== */
+// low frequency callbacks: thread creation etc.
+
+VOID ThreadStart(THREADID tid, CONTEXT *ctxt, INT32 flags, VOID *v)
+{
+    if (tid >= MAX_THREADS) {
+      fprintf(stderr, "ERROR: too many threads (tid %d)\n", tid);
+      exit(1);
+    }
+    //fprintf(stderr, "== ThreadStart %d\n", tid);
+
+    struct ThreadData* d;
+    d = (struct ThreadData*) malloc(sizeof(struct ThreadData));
+
+    for(int i=0; i<MAX_PREDS; i++) {
+        struct AddrPredict* p = &(d->apreds[i]);
+
+        p->currentAddr = 0;
+        p->predictedAddr = 0;
+        p->stride = 0;
+        p->lastAddr = 0;
+        p->lastStride = 0;
+        p->missCount = 0;
+        p->count = 0;
+    }
+
+    for(int i=0; i<MAX_EXITS; i++)
+        d->exit_counter[i] = 0;
+
+    for(int i=0; i<MAX_PREDCOUNTERS; i++)
+        d->predCounters[i] = 0;
+
+    tdata[tid] = d;
+}
+
 
 /* ===================================================================== */
 // Instrumentation
 
 int getAPred(Addr iaddr)
 {
-    if (nextAPred >= MAXPREDS) {
+    if (nextAPred >= MAX_PREDS) {
 	fprintf(stderr, "ERROR: Too many predictors needed\n");
 	exit(1);
     }
-
-    struct AddrPredict* p = &apreds[nextAPred];
-
-    p->predictedAddr = 0;
-    p->stride = 0;
-    p->lastAddr = 0;
-    p->lastStride = 0;
-    p->iaddr = iaddr;
-    p->missCount = 0;
-    p->count = 0;
+    int p = nextAPred;
+    predIAddr[p] = iaddr;
 
     nextAPred++;
-    return nextAPred-1;
+    return p;
 }
 
 int getExit(int from, int to)
@@ -176,7 +210,6 @@ int getExit(int from, int to)
     int c = nextExit;
     exit_from[c] = from;
     exit_to[c] = to;
-    exit_counter[c] = 0;
 
     nextExit++;
     return c;
@@ -214,45 +247,44 @@ int accBase = 0;
 VOID Instruction(INS ins, VOID*)
 {
     for (UINT32 i = 0; i < INS_MemoryOperandCount(ins); i++) {
-	if (INS_MemoryOperandIsRead(ins,i) ||
-	    INS_MemoryOperandIsWritten(ins,i)) {
-	    int off = getAPred(INS_Address(ins));
+        if (INS_MemoryOperandIsRead(ins,i) ||
+                INS_MemoryOperandIsWritten(ins,i)) {
+            int off = getAPred(INS_Address(ins));
 
-	    if (accNumber == 0)
-		INS_InsertCall( ins, IPOINT_BEFORE,
-				(AFUNPTR) returnPredPtrN<0>,
-				IARG_PTR, &apreds[off],
-				IARG_RETURN_REGS, predReg,
-				IARG_END);
+            if (accNumber == 0)
+                INS_InsertCall( ins, IPOINT_BEFORE, (AFUNPTR) returnPredPtr,
+                                IARG_THREAD_ID,
+                                IARG_UINT32, off,
+                                IARG_RETURN_REGS, predReg,
+                                IARG_END);
 
-	    if (accNumber - accBase >= MAX_ADDROFFSET) {
-		INS_InsertCall( ins, IPOINT_BEFORE,
-				(AFUNPTR) returnPredPtrN<MAX_ADDROFFSET>,
-				IARG_REG_VALUE, predReg,
-				IARG_RETURN_REGS, predReg,
-				IARG_END);
-		accBase += MAX_ADDROFFSET;
-	    }
+            if (accNumber - accBase >= MAX_ADDROFFSET) {
+                INS_InsertCall( ins, IPOINT_BEFORE,
+                                (AFUNPTR) returnPredPtrN<MAX_ADDROFFSET>,
+                                IARG_REG_VALUE, predReg,
+                                IARG_RETURN_REGS, predReg,
+                                IARG_END);
+                accBase += MAX_ADDROFFSET;
+            }
 
-	    AFUNPTR p = storeAddrPtr(accNumber - accBase);
-	    INS_InsertCall( ins, IPOINT_BEFORE, p,
-			    IARG_REG_VALUE, predReg,
-			    IARG_MEMORYOP_EA, i,
-			    IARG_END);
+            AFUNPTR p = storeAddrPtr(accNumber - accBase);
+            INS_InsertCall( ins, IPOINT_BEFORE, p,
+                            IARG_REG_VALUE, predReg,
+                            IARG_MEMORYOP_EA, i,
+                            IARG_END);
 
-	    accNumber++;
-	}
+            accNumber++;
+        }
     }
 }
 
 AFUNPTR afterExitPtr(int n)
 {
-  if (n==0) return 0;
-  //return (AFUNPTR) afterExit;
-
 #if DEBUG_STATS
   return (AFUNPTR) afterExit;
 #endif
+
+  if (n==0) return 0;
 
   switch(n) {
   case 1: return (AFUNPTR) afterExitN<1>;
@@ -283,40 +315,46 @@ VOID Trace(TRACE trace, VOID *v)
         }
 
         if (INS_IsBranchOrCall(BBL_InsTail(bbl))) {
-          AFUNPTR p = afterExitPtr(nextAPred-first);
-	  if (p == (AFUNPTR) afterExit) {
-	    BBL_InsertCall( bbl, IPOINT_TAKEN_BRANCH, p,
-                            IARG_UINT32, first,
-                            IARG_UINT32, nextAPred,
+            AFUNPTR p = afterExitPtr(nextAPred-first);
+            if (p == (AFUNPTR) afterExit) {
+                BBL_InsertCall( bbl, IPOINT_TAKEN_BRANCH, p,
+                                IARG_THREAD_ID,
+                                IARG_UINT32, first,
+                                IARG_UINT32, nextAPred,
+                                IARG_END);
+            }
+            else if (p != 0) {
+                BBL_InsertCall( bbl, IPOINT_TAKEN_BRANCH, p,
+                                IARG_THREAD_ID,
+                                IARG_UINT32, first,
+                                IARG_END);
+            }
+            BBL_InsertCall( bbl, IPOINT_TAKEN_BRANCH,
+                            (AFUNPTR) incExitCounter,
+                            IARG_THREAD_ID,
+                            IARG_UINT32, getExit(first, nextAPred),
                             IARG_END);
-	  }
-	  else if (p != 0) {
-	    BBL_InsertCall( bbl, IPOINT_TAKEN_BRANCH, p,
-	                    IARG_UINT32, first,
-                            IARG_END);
-          }
-	  BBL_InsertCall( bbl, IPOINT_TAKEN_BRANCH,
-                          (AFUNPTR) incExitCounter,
-                          IARG_UINT32, getExit(first, nextAPred),
-                          IARG_END);
-	}
+        }
     }
     if (TRACE_HasFallThrough(trace) && (nextAPred > first)) {
-      AFUNPTR p = afterExitPtr(nextAPred-first);
-      if (p == (AFUNPTR) afterExit) {
-        TRACE_InsertCall( trace, IPOINT_AFTER, p,
-			  IARG_UINT32, first,
-			  IARG_UINT32, nextAPred,
+        AFUNPTR p = afterExitPtr(nextAPred-first);
+        if (p == (AFUNPTR) afterExit) {
+            TRACE_InsertCall( trace, IPOINT_AFTER, p,
+                              IARG_THREAD_ID,
+                              IARG_UINT32, first,
+                              IARG_UINT32, nextAPred,
+                              IARG_END);
+        }
+        else if (p != 0) {
+            TRACE_InsertCall( trace, IPOINT_AFTER, p,
+                              IARG_THREAD_ID,
+                              IARG_UINT32, first,
+                              IARG_END);
+        }
+        TRACE_InsertCall( trace, IPOINT_AFTER, (AFUNPTR) incExitCounter,
+                          IARG_THREAD_ID,
+                          IARG_UINT32, getExit(first, nextAPred),
                           IARG_END);
-      }
-      else if (p != 0) {
-        TRACE_InsertCall( trace, IPOINT_AFTER, p,
-			  IARG_UINT32, first,
-                          IARG_END);
-      }
-      TRACE_InsertCall( trace, IPOINT_AFTER, (AFUNPTR) incExitCounter,
-                        IARG_UINT32, getExit(first, nextAPred),
-                        IARG_END);
     }
 }
 
@@ -325,39 +363,64 @@ VOID Fini(int code, VOID * v)
     u64 totalMissCount = 0;
     u64 totalHitCount = 0;
     u64 totalCount = 0;
-    u64 exitCounter = 0;
+    u64 totalCount2 = 0;
 
-    for(int i=0; i<nextExit; i++) {
-      for(int j=exit_from[i]; j<exit_to[i]; j++) {
-	apreds[j].count += exit_counter[i];
-	exitCounter += exit_counter[i];
-      }
+    fprintf(stderr,
+            "==\n"
+            "PinTool Results\n"
+            "  predictors: %d, exits: %d\n",
+            nextAPred, nextExit);
+
+    for(int tid=0; tid<MAX_THREADS; tid++) {
+        struct ThreadData* d = tdata[tid];
+        if (d == 0) continue;
+
+        u64 threadMissCount = 0;
+        u64 threadHitCount = 0;
+        u64 threadCount = 0;
+        u64 threadCount2 = 0;
+        int threadExitsUsed = 0;
+
+        for(int i=0; i<nextExit; i++) {
+            u64 counter = d->exit_counter[i];
+            if (counter == 0) continue;
+            threadExitsUsed++;
+
+            for(int j=exit_from[i]; j<exit_to[i]; j++) {
+                d->apreds[j].count += counter;
+                threadCount2 += counter;
+            }
+        }
+
+        for(int i=0; i<nextAPred; i++) {
+            struct AddrPredict* p = &(d->apreds[i]);
+
+            threadMissCount += p->missCount;
+            threadCount += p->count;
+        }
+        threadHitCount = threadCount - threadMissCount;
+
+        totalMissCount += threadMissCount;
+        totalHitCount += threadHitCount;
+        totalCount += threadCount;
+        totalCount2 += threadCount2;
+
+        fprintf(stderr, "  Thread %d\n", tid);
+        fprintf(stderr, "    predicted: %llu / %llu = %5.2f %%\n",
+                threadHitCount, threadCount,
+                100.0 * (double)threadHitCount / (double)threadCount);
+        fprintf(stderr, "    (exits used: %d, counters: %llu)\n",
+                threadExitsUsed, threadCount2);
+
+        for(int i=0; i<MAX_PREDCOUNTERS; i++) {
+            if (d->predCounters[i] == 0) continue;
+            fprintf(stderr, "   PredCounters %2d : %llu\n",
+                    i, d->predCounters[i]);
+        }
     }
-
-    for(int i=0; i<nextAPred; i++) {
-	struct AddrPredict* p = &apreds[i];
-
-//	if (p1->hitCount + p2->missCount > 1000)
-//	    fprintf(stderr, "%5i: I %lx, %d/%d\n",
-//		    i, p2->iaddr, p1->hitCount, p1->hitCount + p2->missCount);
-
-	totalMissCount += p->missCount;
-	totalCount += p->count;
-    }
-    totalHitCount = totalCount - totalMissCount;
-
-#if DEBUG_STATS
-    for(int i=0; i<MAX_PREDCOUNTERS; i++) {
-      if (predCounters[i] == 0) continue;
-      fprintf(stderr, " PredCounters %2d : %llu\n", i, predCounters[i]);
-    }
-#endif
-
-    fprintf(stderr, "Predicted: %llu / %llu = %5.2f %%\n",
+    fprintf(stderr, "  Total predicted: %llu / %llu = %5.2f %%\n",
             totalHitCount, totalCount,
-	    100.0 * (double)totalHitCount / (double)totalCount);
-    fprintf(stderr, " predictors: %d, trace exits: %d, exit counter: %llu\n",
-            nextAPred, nextExit, exitCounter);
+            100.0 * (double)totalHitCount / (double)totalCount);
 }
 
 /* ===================================================================== */
@@ -372,17 +435,15 @@ int main(int argc, char *argv[])
       return 0;
     }
 
-    TRACE_AddInstrumentFunction(Trace, 0);
-    //INS_AddInstrumentFunction(Instruction, 0);
-    PIN_AddFiniFunction(Fini, 0);
-
     predReg = PIN_ClaimToolRegister();
     storeFuncGen<MAX_STOREFUNC-1>();
 
-#if DEBUG_STATS
-    for(int i=0; i<MAX_PREDCOUNTERS; i++)
-      predCounters[i] = 0;
-#endif
+    for(int i=0; i<MAX_THREADS; i++)
+      tdata[i] = 0;
+
+    TRACE_AddInstrumentFunction(Trace, 0);
+    PIN_AddFiniFunction(Fini, 0);
+    PIN_AddThreadStartFunction(ThreadStart, 0);
 
     // Never returns
     PIN_StartProgram();
